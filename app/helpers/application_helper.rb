97da6ae6238171c69b0f16ba971ab9fc41552779
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-01 22:53:28 +0300

    Create login_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    if session[:current_user_id].nil?
+      link_to 'login', login_path
+      link_to 'Signup', signup_path
+    else
+      session[:current_user_fullname]
+      link_to 'log out', logout_path
+    end
+  end
 end
