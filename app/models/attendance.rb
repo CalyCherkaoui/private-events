606cc1f8d4d835579d8f87aea37ae4fae97a241f
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-01 17:18:59 -0500

    Adding attendance reltionship

--- app/models/attendance.rb
+++
@@ -1,6 +0,0 @@
-class Attendance < ApplicationRecord
-    #belongs_to :user, foreign_key: :attendee_id, class_name: 'User'
-    #belongs_to :event, foreign_key: :attended_event_id, class_name: 'Event'
-    belongs_to :attendee, foreign_key: 'attendee_id', class_name: 'User'
-    belongs_to :attended_event, foreign_key: 'attended_event_id', class_name: 'Event'
-end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
-    has_many :attendees, through: :attendance, source: :user
+    has_many :attendences
+    has_many :attendees, through: :attendences, source: :user
 end
