ad8af63aed41f32310b648e340f47525dcb84b7c
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-02 22:31:26 +0300

    Refactor the “upcoming” and “past” methods into simple scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
-    belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
-    has_many :attendences
-    has_many :attendees, through: :attendences, source: :user
+  belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
+  has_many :attendences
+  has_many :attendees, through: :attendences, source: :user
+
+  scope :past, -> { where('event_date < ?', Time.now).to_a }
+  scope :upcoming, -> { where('event_date > ?', Time.now).to_a }
 end
